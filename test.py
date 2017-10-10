a65bf81a33e4a8cd4544d105b1355d7ffe95f8f8
Jay Heo <heojae91@gmail.com>
2017-10-10 19:26:43 +0900

    modulator modifying

--- test.py
+++ test.py
@@ -29,7 +29,9 @@ volume = 0.5        # Set volume in 0.0 ~ 1.0 scale
 fs = 48000          # Set sample rate 48000 Hz
 duration = 1.0      # Set duration for 1 second
 f = 20000.0         # Set carrier frequency in float
-baudRate = 4000.0   # Set baud rate as 4000 Hz
+baudRate = 4000   # Set baud rate as 4000 Hz
+samplesPerBaud = fs / baudRate
+signalPerBaud = int(f / baudRate)
 
 # Get sinusoid signal point in every point as float type
 samples = []
@@ -38,9 +40,10 @@ prev = 0
 # Get sign value on array
 signArray = []
 # Generate samples
-for i in range(20) :
+for i in range(fs) :
     currentSign = 1                 # 1 for 1, -1 for 0. Basically set to 1
-    currentIndex = int((i+1) / 2.4) # Update index to scale of carrier frequency
+    currentIndex = int((i / samplesPerBaud) / signalPerBaud)
+    # currentIndex = int((i+1) / 2.4) # Update index to scale of carrier frequency
 
     if (currentIndex != prev) :
         if (trainingSequence[int(currentIndex / 50) % 8][currentIndex % 50] == 1) :
@@ -50,6 +53,7 @@ for i in range(20) :
         prev = currentIndex
     # To examine sign
     # signArray.append(currentSign)
+    print(currentIndex)
     print("Row : ", int(currentIndex / 50) % 8, ", Column : ", currentIndex % 50)
     samples.append(np.sin(2 * np.pi * i * f / fs * currentSign).astype(np.float32))
 
