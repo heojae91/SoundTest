7b57e6167634506f5d392c6b11ba40c6f2b1bd6b
heojae91 <heojae91@gmail.com>
2017-10-10 16:00:41 +0900

    modified Sine Wave generator

--- SineWave.py
+++ SineWave.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-np.pi, np.pi, 201)
+x = np.linspace(0, 12 * np.pi, 1000)
 plt.plot(x, np.sin(x))
 plt.xlabel("Angle [rad]")
 plt.ylabel("sin(x)")
 plt.axis('tight')
-plt.show()
+plt.savefig("result/sinewave.png")
