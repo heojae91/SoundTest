dba09b13ab0264be349fd4a8c6b93119338a894c
Jay Heo <heojae91@gmail.com>
2017-10-13 22:06:24 +0900

    generating modulator

--- Modulator.py
+++ Modulator.py
@@ -6,7 +6,7 @@ def plotingSignal(object, filename) :
     fig = plt.figure()
 
     axes = plt.gca()
-    axes.set_ylim([-1.0, 1.0])
+    axes.set_ylim([-1.5, 1.5])
     axes.set_xlim([0.0, object.__len__()])
 
     s = fig.add_subplot(111)
@@ -14,6 +14,12 @@ def plotingSignal(object, filename) :
     s.plot(amplitude)
     fig.savefig("result/"+filename)
     fig.clear()
+
+def upconverter(sampArray, carrierFreq) :
+    for i in range(sampArray.__len__()) :
+        sampArray[i] = sampArray[i] * np.cos(2 * np.pi * carrierFreq * i / fs) * (2 ** (1/2))
+    for i in range(10) :
+        print(sampArray[i])
 
 p = pyaudio.PyAudio()
 
@@ -31,6 +37,7 @@ volume = 0.5        # Set volume in 0.0 ~ 1.0 scale
 fs = 48000          # Set sample rate 48000 Hz
 duration = 1.0      # Set duration for 1 second
 f = 4000.0         # Set carrier frequency in float
+samplesPerBaud = fs / f
 
 # Get sinusoid signal point in every point as float type
 samples = []
@@ -38,10 +45,10 @@ prev = -1
 currentSign = 0 # 1 for 1, -1 for 0. Basically set to 1
 # Get sign value on array
 signArray = []
+
 # Generate samples
 for i in range(int(duration * fs)) :
-    print(i)
-    currentIndex = int(i / f)
+    currentIndex = int(i / samplesPerBaud)
 
     if (currentIndex != prev) :
         if (trainingSequence[int(currentIndex / 50) % 8][currentIndex % 50] == 1) :
@@ -51,11 +58,13 @@ for i in range(int(duration * fs)) :
         prev = currentIndex
 #    print("Row : ", int(currentIndex / 50) % 8, ", Column : ", currentIndex % 50)
     print(currentIndex)
+    signArray.append(currentSign)
     samples.append(np.sin(2 * np.pi * i * f / fs * currentSign).astype(np.float32))
-samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
 
-#standardSound = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+standardSound = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
 
+upconverter(samples, 20000)
+print("maximum : ", np.amax(samples))
 samples = np.array(samples)
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
@@ -63,11 +72,11 @@ stream = p.open(format=pyaudio.paFloat32,
                 output=True)
 
 stream.write(volume * samples)
-"""
+print(2 ** (1/2))
 for i in range(100,fs,100) :
     plotingSignal(samples[i-100:i], str(i) + 'plt.png')
     plotingSignal(standardSound[i-100:i], str(i) + 'std.png') 
-"""
+
 stream.stop_stream()
 stream.close()
 
