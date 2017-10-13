3f40bd6298ef403726372738faaa6546c3a6d64b
heojae91 <heojae91@gmail.com>
2017-10-14 07:09:41 +0900

    testing modulator

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 def plotingSignal(object, filename) :
     fig = plt.figure()
 
-    axes = plt.gca()
-    axes.set_ylim([-1.0, 1.0])
-    axes.set_xlim([0.0, object.__len__()])
+    # axes = plt.gca()
+    # axes.set_ylim([-1.5, 1.5])
+    # axes.set_xlim([0.0, object.__len__()])
 
     s = fig.add_subplot(111)
     amplitude = np.fromstring(object, np.float32)
@@ -15,59 +15,16 @@ def plotingSignal(object, filename) :
     fig.savefig("result/"+filename)
     fig.clear()
 
-p = pyaudio.PyAudio()
+sinSig = (np.sin(np.arange(48000*1.0)*4000.0/48000)).astype(np.float32)
+cosSig = (2 ** (1/2) * (np.cos(2*np.pi*np.arange(48000*1.0)*4000.0/48000)).astype(np.float32))
 
-# 26-bit GSM Training Sequence
-trainingSequence = ((0,0,1,0,0,1,0,1,1,1,0,0,0,0,1,0,0,0,1,0,0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                    (0,0,1,0,1,1,0,1,1,1,0,1,1,1,1,0,0,0,1,0,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                    (0,1,0,0,0,0,1,1,1,0,1,1,1,0,1,0,0,1,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                    (0,1,0,0,0,1,1,1,1,0,1,1,0,1,0,0,0,1,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                    (0,0,0,1,1,0,1,0,1,1,1,0,0,1,0,0,0,0,0,1,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                    (0,1,0,0,1,1,1,0,1,0,1,1,0,0,0,0,0,1,0,0,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                    (1,0,1,0,0,1,1,1,1,1,0,1,1,0,0,0,1,0,1,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                    (1,1,1,0,1,1,1,1,0,0,0,1,0,0,1,0,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+plotingSignal(sinSig[:200], "sine_sig")
+plotingSignal(cosSig[:200], "cosine_sig")
 
-volume = 0.5        # Set volume in 0.0 ~ 1.0 scale
-fs = 48000          # Set sample rate 48000 Hz
-duration = 2.0      # Set duration for 1 second
-f = 20000.0         # Set carrier frequency in float
-baudRate = 4000   # Set baud rate as 4000 Hz
-samplesPerBaud = fs / baudRate
+upconvertedSig = np.convolve(sinSig, cosSig)
 
-# Get sinusoid signal point in every point as float type
-samples = []
-prev = -1
-currentSign = 0 # 1 for 1, -1 for 0. Basically set to 1
-# Get sign value on array
-signArray = []
-# Generate samples
-for i in range(int(duration * fs)) :
-    currentIndex = int(i / samplesPerBaud)
+plotingSignal(upconvertedSig[:200], "upconverted")
 
-    if (currentIndex != prev) :
-        if (trainingSequence[int(currentIndex / 50) % 8][currentIndex % 50] == 1) :
-            currentSign = 1
-        else :
-            currentSign = -1
-        prev = currentIndex
-#    print("Row : ", int(currentIndex / 50) % 8, ", Column : ", currentIndex % 50)
-    samples.append(np.sin(2 * np.pi * i * f / fs * currentSign).astype(np.float32))
+downconvertedSig = np.convolve(upconvertedSig, cosSig)
 
-#standardSound = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
-
-samples = np.array(samples)
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
-
-stream.write(volume * samples)
-"""
-for i in range(100,fs,100) :
-    plotingSignal(samples[i-100:i], str(i) + 'plt.png')
-    plotingSignal(standardSound[i-100:i], str(i) + 'std.png') 
-"""
-stream.stop_stream()
-stream.close()
-
-p.terminate()
+plotingSignal(downconvertedSig[:200], "downconverted")
