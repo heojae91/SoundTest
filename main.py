75d753f90e845eea1af574967107862b436363ac
Jay Heo <heojae91@gmail.com>
2017-10-17 16:36:43 +0900

    Filter applied

--- main.py
+++ main.py
@@ -1,38 +1,11 @@
 import numpy as np
 import pyaudio
 from scipy import fft
-from scipy.signal import butter, lfilter, freqz
 import matplotlib.pyplot as plt
 
 import Modulator
 import Demodulator
-
-def butter_bandpass(lowcut, highcut, fs, order=5) :
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low,high], btype='bandpass')
-    return b, a
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5) :
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = signal.lfilter(b, a, data)
-    return y
-
-def run_filter(signal, fs, lowcut, highcut) :
-    T = 1.0
-    nsamples = T * fs
-    t = np.linspace(0, T, nsamples, endpoint=False)
-    y = butter_bandpass_filter(signal, lowcut, highcut, fs, order=6)
-    plt.plot(t, y, label='Filtered signal (%g Hz)' % fs)
-    plt.xlabel('time (seconds)')
-    plt.hlines([-0.02, 0.02], 0, T, linestyles='--')
-    plt.grid(True)
-    plt.axis('tight')
-    plt.legend(loc='upper left')
-
-    plt.show()
-
+import Filter
 
 def plotSpectrum(y, fs) :
     n = len(y)
@@ -69,11 +42,10 @@ fs = 48000
 ts = 1.0/fs
 t = np.arange(0, 1, ts)
 
-run_filter(signal, fs, lowcut=18000, highcut=22000)
-plotSpectrum(signal, fs)
+filteredSignal = Filter.butter_bandpass_filter(signal, lowcut=18000, highcut=22000, fs=fs)
+plotSpectrum(filteredSignal, fs)
 plt.show()
 
-run_filter(48000, 18000.0, 22000.0)
 volume = 0.5
 
 signal = np.array(signal)
@@ -82,8 +54,10 @@ stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=fs,
                 output=True)
-
-stream.write(volume * signal)
+plt.clf()
+plt.plot(filteredSignal[100:200])
+plt.show()
+stream.write(volume * filteredSignal)
 
 stream.stop_stream()
 stream.close()
