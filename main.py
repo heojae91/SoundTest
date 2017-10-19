432d61725f542f5f32ed91572e7eeb1ca16dc97a
heojae91 <heojae91@gmail.com>
2017-10-19 21:52:41 +0900

    modifying modulator

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ trainingSequence = ((0,0,1,0,0,1,0,1,1,1,0,0,0,0,1,0,0,0,1,0,0,1,0,1,1,1,0,0,0,0
 
 signal = Modulator.generateSequence(trainingSequence)
 convolvedSignal = Modulator.convolveSignal(signal)
+signArray = Modulator.generateSignSequence(trainingSequence)
 
 p = pyaudio.PyAudio()
 p_receiver = pyaudio.PyAudio()
@@ -35,20 +36,19 @@ t = np.arange(0, 1, ts)
 filteredSignal = Filter.butter_bandpass_filter(convolvedSignal, lowcut=18000, highcut=22000, fs=fs)
 #Plotter.plotSpectrum(filteredSignal, fs)
 convolvedSignal= Modulator.convolveSignal(filteredSignal)
-# filteredSignal = Filter.butter_bandpass_filter(convolvedSignal, lowcut=2000, highcut=6000, fs = fs)
+#filteredSignal = Filter.butter_bandpass_filter(convolvedSignal, lowcut=2000, highcut=6000, fs = fs)
 #Plotter.plotSpectrum(filteredSignal, fs)
-plt.show()
 
-print(Modulator.generateSignSequence(trainingSequence))
 volume = 0.5
-print("Signal : ", signal[0:12])
 Demodulator.demod(filteredSignal)
+Plotter.plotSpectrum(convolvedSignal, fs)
+
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=fs,
                 output=True)
 # plt.clf()
-# plt.plot(convolvedSignal[0:500])
+# plt.plot(convolvedSignal)
 # plt.show()
 stream.write(volume * filteredSignal)
 
