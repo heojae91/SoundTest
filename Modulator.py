6a307273494a78bafe56d9de7f46023258e692d8
Jay Heo <heojae91@gmail.com>
2017-10-19 15:51:10 +0900

    Modifying modulator

--- Modulator.py
+++ Modulator.py
@@ -19,20 +19,22 @@ def plotingSignal(object, filename) :
 def upconverter(sampArray, carrierFreq) :
     cosSig = []
 
-    # Generate cosine signal to convolve with sampled signal
-    # for i in range(sampArray.__len__()) :
-    #     cosSig.append((2 ** (1/2)) * 2 * np.pi * i / )
-    # for i in range(10) :
-    #     print(sampArray[i])
+def generateSignSequence(sequence, f=4000, fs=48000) :
+    samplesPerSymbol = fs / f
+    signArray = []
+    for i in range(fs) :
+        if (sequence[i % (50 * samplesPerSymbol)][i % samplesPerSymbol] == 1) :
+            signArray.append(1)
+        else :
+            signArray.append(-1)
+    return signArray
 
-p = pyaudio.PyAudio()
-
-volume = 0.5  # Set volume in 0.0 ~ 1.0 scale
-fs = 48000  # Set sample rate 48000 Hz
-duration = 1.0  # Set duration for 1 second
-f = 4000.0  # Set carrier frequency in float
 
 def generateSequence(sequence) :
+    fs = 48000  # Set sample rate 48000 Hz
+    duration = 1.0  # Set duration for 1 second
+    f = 4000.0  # Set carrier frequency in float
+
     # Get sinusoid signal point in every point as float type
     samples = []
     prev = -1
@@ -40,44 +42,20 @@ def generateSequence(sequence) :
     # Get sign value on array
     signArray = []
     # Generate samples
-    for i in range(int(duration * fs)):
-        currentIndex = int(i / f)
-
+    for i in range(int(duration * fs)) :
+        currentIndex = int(i / (fs/f))
         if (currentIndex != prev):
             if (sequence[int(currentIndex / 50) % 8][currentIndex % 50] == 1):
                 currentSign = 1
+                signArray.append(currentSign)
             else:
                 currentSign = -1
+                signArray.append(currentSign)
             prev = currentIndex
             #    print("Row : ", int(currentIndex / 50) % 8, ", Column : ", currentIndex % 50)
-        samples.append(np.cos(2 * np.pi * i * f / fs * currentSign).astype(np.float32))
-    cosineSignal = np.cos(2 * np.pi * np.arange(1.0 * fs) * 20000 / fs)
-    return samples * cosineSignal
+        samples.append(np.sin(2 * np.pi * i * f / fs * currentSign).astype(np.float32))
+    return samples
 
-# volume = 0.5  # Set volume in 0.0 ~ 1.0 scale
-# fs = 48000  # Set sample rate 48000 Hz
-# duration = 1.0  # Set duration for 1 second
-# f = 20000.0  # Set carrier frequency in float
-# baudRate = 4000  # Set baud rate as 4000 Hz
-# samplesPerBaud = fs / baudRate
-# def generateSequence(sequence) :
-#     trainingSequence = sequence
-#     # Get sinusoid signal point in every point as float type
-#     samples = []
-#     prev = -1
-#     currentSign = 0 # 1 for 1, -1 for 0. Basically set to 1
-#     # Get sign value on array
-#     signArray = []
-#     # Generate samples
-#     for i in range(int(duration * fs)) :
-#         currentIndex = int(i / samplesPerBaud)
-#
-#         if (currentIndex != prev) :
-#             if (trainingSequence[int(currentIndex / 50) % 8][currentIndex % 50] == 1) :
-#                 currentSign = 1
-#             else :
-#                 currentSign = -1
-#             prev = currentIndex
-#     #    print("Row : ", int(currentIndex / 50) % 8, ", Column : ", currentIndex % 50)
-#         samples.append(np.sin(2 * np.pi * i * f / fs * currentSign).astype(np.float32))
-#     return samples
+def convolveSignal(signal, fs=48000) :
+    cosineSignal = 2 ** (1 / 2) * np.cos(2 * np.pi * np.arange(1.0 * fs) * 16000 / fs)
+    return signal * cosineSignal
