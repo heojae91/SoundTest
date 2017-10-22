d4bf3e1ca74fb49a6cd27bfa96ebe27733afff1a
heojae91 <heojae91@gmail.com>
2017-10-23 08:51:31 +0900

    Generating demodulator

--- Demodulator.py
+++ Demodulator.py
@@ -2,6 +2,18 @@ import numpy as np
 import pyaudio
 import matplotlib.pyplot as plt
 
+import Modulator
+
+"""
+아리까리한점
+1. 어떻게 스트림과 디모듈레이션을 동시에 수행할 수 있을까 
+진행 스텝
+1. 일단 수신한다.
+2. 수신한 신호를 비트스트림으로 변경한다.
+3. 비트 스트림을 분석하여 레퍼런스 시그널(트레이닝 시퀀스)와 일치시킨다.
+4. phase를 비교한다
+"""
+
 volume = 0.5        # Set volume in 0.0 ~ 1.0 scale
 fs = 48000          # Set sample rate 48000 Hz
 duration = 2.0      # Set duration for 1 second
@@ -10,24 +22,8 @@ baudRate = 4000   # Set baud rate as 4000 Hz
 CHUNKSIZE = 1024 # Size of chunk
 
 # Get bit sequence from signal
-def downconverter(signal, cosSignal) :
-    return signal * cosSignal
+def downconverter(signal) :
+    return Modulator.upconvertSignal(signal)
 
 def demod(samples) :
     print("Samples : ", samples[0:12])
-
-    # p = pyaudio.PyAudio()
-    # stream = p.open(format=pyaudio.paFloat32,
-    #                 channels=1,
-    #                 rate=48000,
-    #                 input=True,
-    #                 frames_per_buffer=CHUNKSIZE)
-    # data = stream.read(CHUNKSIZE)
-    # numpydata = np.fromstring(data, dtype=np.float32)
-    #
-    # stream.stop_stream()
-    # stream.close()
-    # p.terminate()
-    #
-    # print(numpydata)
-    # return
