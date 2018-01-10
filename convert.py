4095bcfae9764bf31f0146d12050e0b39128cf41
Jay Heo <heojae91@gmail.com>
2018-01-10 21:20:51 +0900

    PCM conversion

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import wave
 import array
 import struct
+import time
 
 
 def convert(fin, fout, chunk_size=1024*1024) :
@@ -9,17 +10,15 @@ def convert(fin, fout, chunk_size=1024*1024) :
     waveout = wave.open(fout, "wb")
     waveout.setparams((1, 1, 48000, 0, "NONE", ""))
 
-    tmp = struct.pack('f', 3.1415982)
-    print(tmp)
-    print(type(tmp))
-
     while True:
         raw_floats = fin.read(chunk_size)
         if raw_floats == "":
             return
-        floats = array.array('f', raw_floats)
-        samples = [sample * 32767
+
+        floats = array.array('f', raw_floats[:len(raw_floats)-2])
+        samples = [int(sample)* 32767
                    for sample in floats]
+        print(type(samples[1]))
         raw_ints = struct.pack("<%dh" % len(samples), *samples)
         waveout.writeframes(raw_ints)
 
